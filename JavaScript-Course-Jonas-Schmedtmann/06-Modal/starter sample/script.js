5ad41eb7bc9e8b518cfdc88ade01074e69a07b98
desnor303 <desmonor@gmail.com>
2023-09-24 19:52:01 +0700

    complete "Modal" project

--- JavaScript-Course-Jonas-Schmedtmann/06-Modal/starter sample/script.js
+++ JavaScript-Course-Jonas-Schmedtmann/06-Modal/starter sample/script.js
@@ -1,1 +1,30 @@
 'use strict';
+// click btn event :
+// 1. 3 modal with a same function
+// Object
+const btn = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const close_btn = document.querySelector('.close-modal');
+// function
+const show = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+console.log(document);
+const hidden = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+// process
+// show
+for (let i = 0; i < btn.length; i++) btn[i].addEventListener('click', show);
+//hidden
+close_btn.addEventListener('click', hidden);
+overlay.addEventListener('click', hidden);
+document.addEventListener('keydown', event => {
+  if (event.key === 'Escape') {
+    hidden();
+  }
+});
