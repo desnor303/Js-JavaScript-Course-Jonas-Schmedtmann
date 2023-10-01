b6f93ae2f41825d134caa95390caf463f8076309
desnor303 <desmonor@gmail.com>
2023-10-01 14:22:31 +0700

    The this Keyword in Practice

--- JavaScript-Course-Jonas-Schmedtmann/08-Behind-the-Scenes/Clone/script.js
+++ JavaScript-Course-Jonas-Schmedtmann/08-Behind-the-Scenes/Clone/script.js
@@ -1,5 +1,7 @@
 'use strict';
+/*
 // Variable
+
 console.log(me);
 console.log(job);
 console.log(year);
@@ -9,3 +11,45 @@ let job = 'engineer';
 const year = 1996;
 
 // Function
+*/
+// This keyword
+console.log(this);
+
+function getAge(thisYear) {
+  let year = 1996;
+  let age = thisYear - year;
+  console.log(age);
+  console.log(this);
+  return age;
+}
+getAge(2023);
+
+let getAgeArrow = thisYear => {
+  let year = 1996;
+  let age = thisYear - year;
+  console.log(age);
+  console.log(this);
+  return age;
+};
+
+getAgeArrow(2023);
+
+const dat = {
+  name: 'Dat',
+  birthYear: 1996,
+  money: 1000000,
+  birthDay: function () {
+    return `${this.name}'s birthDay is ${this.birthYear}`;
+  },
+};
+
+const getBirth = dat.birthDay;
+
+const BaoYen = {
+  name: 'BaoYen',
+  birthYear: 1995,
+  money: 1000000,
+};
+
+BaoYen.birthDay = getBirth;
+console.log(BaoYen.birthDay());
