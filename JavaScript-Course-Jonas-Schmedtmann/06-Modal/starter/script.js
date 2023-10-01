0c97e15f7962a835e84f8fee042aec4a0a86d1ff
desnor303 <desmonor@gmail.com>
2023-10-01 13:07:29 +0700

    "Hoisting and TDZ in Practice"

--- JavaScript-Course-Jonas-Schmedtmann/06-Modal/starter/script.js
+++ JavaScript-Course-Jonas-Schmedtmann/06-Modal/starter/script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+let test2 = () => {
+  let localVariable = 'local';
+  var testlocal1 = 'var1';
+  testlocal1;
+};
+test2();
+console.log(testlocal1);
---
+++ JavaScript-Course-Jonas-Schmedtmann/08-Behind-the-Scenes/Clone/script.js
@@ -0,0 +1,11 @@
+'use strict';
+// Variable
+console.log(me);
+console.log(job);
+console.log(year);
+
+var me = 'Dat';
+let job = 'engineer';
+const year = 1996;
+
+// Function
