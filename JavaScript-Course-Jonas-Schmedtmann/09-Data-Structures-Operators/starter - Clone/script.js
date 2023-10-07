6636eae6a21c681322e09bb6c0d0de1da8780803
desnor303 <desmonor@gmail.com>
2023-10-07 14:33:48 +0700

    Coding Challenge #4

--- JavaScript-Course-Jonas-Schmedtmann/09-Data-Structures-Operators/starter - Clone/script.js
+++ JavaScript-Course-Jonas-Schmedtmann/09-Data-Structures-Operators/starter - Clone/script.js
@@ -639,6 +639,7 @@ Let's continue with our football betting app! This time, we have a map with a lo
 
 GOOD LUCK 😀
 */
+/*
 
 const gameEvents = new Map([
   [17, '⚽️ GOAL'],
@@ -670,3 +671,56 @@ for (let [key, value] of gameEvents) {
   let half = key < 45 ? '[FIRST HALF]' : '[SECOND HALF]';
   console.log(`${half} key: ${value}`);
 }
+
+*/
+//todo Coding Challenge #4
+
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', function () {
+  let text = document.querySelector('textarea').value;
+
+  function camelCase(text) {
+    let rows = text.toLowerCase().split('\n');
+    for (let [index, row] of rows.entries()) {
+      let [first, second] = row.trim().split('_');
+      let output =
+        (first + second.replace(second[0], second[0].toUpperCase())).padEnd(
+          20,
+          ' '
+        ) + '✅'.repeat(index + 1);
+      console.log(output);
+    }
+  }
+  camelCase(text);
+});
