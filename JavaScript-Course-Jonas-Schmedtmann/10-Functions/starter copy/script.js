a106174f56681985113ff5794da098198ce0dd69
desnor303 <desmonor@gmail.com>
2023-10-07 15:43:12 +0700

    Function: "default parameter"

---
+++ JavaScript-Course-Jonas-Schmedtmann/10-Functions/starter copy/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+//todo default parameter
+/*
+const bookings = [];
+const creatBooking = function (
+  flightNum,
+  numPassenger = 1,
+  price = 199 * numPassenger
+) {
+  const booking = {
+    flightNum,
+    numPassenger,
+    price,
+  };
+  bookings.push(booking);
+};
+
+creatBooking('b123', undefined, 200);
+console.log(bookings);
+
+*/
